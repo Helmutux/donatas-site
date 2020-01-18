a7fc9957e1246edf97f609adbbf79da884bffd06
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-01-18 21:14:50 +0200

    Add files via upload

--- source/carspeed/script.js
+++ source/carspeed/script.js
@@ -9,13 +9,22 @@ var form = document.querySelector('.uzklausa');
 form.onsubmit = function (evt) {
     // instrukcija atsaukia duomenu issiuntima
     evt.preventDefault();
-    metrai = Number(document.getElementById('atstumas').value);
-    sekundes = Number(document.getElementById('laikas').value);
-    
 
-    speed = ((3600 * metrai)/ sekundes) / 1000;
+    kilometrai = Number(document.getElementById('kilometrai').value);
+    metrai = Number(document.getElementById('metrai').value);
+    valandos = Number(document.getElementById('hours').value);
+    minutes = Number(document.getElementById('minutes').value);
+    sekundes = Number(document.getElementById('seconds').value);
+    var atstumas = kilometrai * 1000 + metrai;
+    var laikas = valandos * 3600 + minutes * 60 + sekundes;
     
-    console.log(speed);
+    // console.log(atstumas);
+    // console.log(laikas);
+
+    speed = (3600 * atstumas / laikas) / 1000;
+
+    
+    // console.log(speed);
 
     cele.innerHTML = speed.toFixed(0);
 
