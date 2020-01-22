1e3ceca71bb48fab81e9e7cde98a38dbb42f910c
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-01-22 22:30:53 +0200

    Add files via upload

--- source/FigureObjectTable/script.js
+++ source/FigureObjectTable/script.js
@@ -1,23 +1,20 @@
 //susikuriame kintamuosisu
-var l;//ilgis
-var h;//aukstis
-var w;//plotis
-
 var objektai = new Array();//masyvas. cia susikelsime objektus t.y. visu figuru duomenis
 
 //paimame duomenis is formos
 var form = document.querySelector('form');
+var volume;
+var volumeArray = [];
 
 form.onsubmit = function (evt) {
     // instrukcija atsaukia duomenu issiuntima
     evt.preventDefault();
 
-
-    l = document.getElementById('ilgis').value;//paimame ilgio reiksme
+    var l = document.getElementById('ilgis').value;//paimame ilgio reiksme
     document.getElementById('ilgis').value = '';//graziname default reiksme iterpimo laukui
-    h = document.getElementById('aukstis').value;//paimame aukscio reiksme
+    var h = document.getElementById('aukstis').value;//paimame aukscio reiksme
     document.getElementById('aukstis').value = '';//graziname default reiksme iterpimo laukui
-    w = document.getElementById('plotis').value;//paimame plocio reiksme
+    var w = document.getElementById('plotis').value;//paimame plocio reiksme
     document.getElementById('plotis').value = '';//graziname default reiksme iterpimo laukui
 
     if ((l === 0 || l === '') || (h === 0 || h === '') || (w === 0 || w === '')) {
@@ -33,50 +30,77 @@ form.onsubmit = function (evt) {
 
         //dedame gauta objekta su reiksmemis i masyva
         objektai.push(objektas);
-        
-        console.log('objektas', objektas);
-        // console.log(objektas);
-        console.log('masyvas', objektai);
-        // console.log(objektai);
-
-
-
 
         for (i = 0; i < objektai.length; i++) {
             var length = objektai[i].ilgis;
             var heigth = objektai[i].aukstis;
             var width = objektai[i].plotis;
-            }
-            //gautai duomenimis pildome lenteles eilute
-            var tbody = document.getElementById('tablebody');
-
-            var tr = document.createElement('tr');
-            tbody.appendChild(tr);
-
-            var td = document.createElement('td');
-            tr.appendChild(td);
-            var txt = document.createTextNode(i);
-            td.appendChild(txt);
-
-            var td = document.createElement('td');
-            tr.appendChild(td);
-            var txt = document.createTextNode(length);
-            td.appendChild(txt);
-
-            var td = document.createElement('td');
-            tr.appendChild(td);
-            var txt = document.createTextNode(heigth);
-            td.appendChild(txt);
-
-            var td = document.createElement('td');
-            tr.appendChild(td);
-            var txt = document.createTextNode(width);
-            td.appendChild(txt);
-
-
+            
+            volume = length * heigth * width;
         }
+        volumeArray.push(volume);
+        // console.log('turis', volume);
+        
+        //gautai duomenimis pildome lenteles eilute
+        var tbody = document.getElementById('tablebody');
+
+        var tr = document.createElement('tr');
+        tbody.appendChild(tr);
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        var txt = document.createTextNode(i);
+        td.appendChild(txt);
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        var txt = document.createTextNode(length);
+        td.appendChild(txt);
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        var txt = document.createTextNode(heigth);
+        td.appendChild(txt);
+
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        var txt = document.createTextNode(width);
+        td.appendChild(txt);
     }
+}
+// console.log('masyvas', volumeArray);
+// console.log(objektai);
 
 
+var maxButton = document.querySelector('#max');
 
+function maxima() {
+    // let emptyArray = [];
+    // let nonExistantArray = undefined; 
+    // if((objektai.isArray(emptyArray)) || (objektai.isArray(nonExistantArray))){
+    //     alert('Norėdami palyginti, suveskite keletą figūrų duomenų');
+    // } else {
+    for (i = 0; i < volumeArray.length; i++) {
+        var vol = volumeArray[i];
+    
+    
+    // var volume = length * heigth * width;
+    //gautai duomenimis pildome lenteles eilute
+    var tbody = document.getElementById('tablebodyMax');
 
+    var tr = document.createElement('tr');
+    tbody.appendChild(tr);
+
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    var txt = document.createTextNode(i);
+    td.appendChild(txt);
+
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    var txt = document.createTextNode(vol);
+    td.appendChild(txt);
+
+}
+
+}
