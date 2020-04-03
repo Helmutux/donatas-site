1830bed05914e204cdd3a2cda9a30dbaf1d5b0e2
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-04-03 20:58:26 +0300

    0402 diena + hide funkcija
    - Prideta 04.02 dienos aprasymas
    - prideta js failas su script'u leidzianciu slepti/rodyti pasirinkta diena

---
+++ source/Karantinas/myscript.js
@@ -0,0 +1,110 @@
+var but001 = document.querySelector('#d001');
+var inf001 = document.querySelector('.inf001');
+
+but001.onclick = function () {
+  inf001.classList.toggle('hide');
+};
+
+var but002 = document.querySelector('#d002');
+var inf002 = document.querySelector('.inf002');
+but002.onclick = function () {
+  inf002.classList.toggle('hide');
+};
+
+var but003 = document.querySelector('#d003');
+var inf003 = document.querySelector('.inf003');
+but003.onclick = function () {
+  inf003.classList.toggle('hide');
+};
+
+var but004 = document.querySelector('#d004');
+var inf004 = document.querySelector('.inf004');
+but004.onclick = function () {
+  inf004.classList.toggle('hide');
+};
+
+var but005 = document.querySelector('#d005');
+var inf005 = document.querySelector('.inf005');
+but005.onclick = function () {
+  inf005.classList.toggle('hide');
+};
+
+var but006 = document.querySelector('#d006');
+var inf006 = document.querySelector('.inf006');
+but006.onclick = function () {
+  inf006.classList.toggle('hide');
+};
+
+var but007 = document.querySelector('#d007');
+var inf007 = document.querySelector('.inf007');
+but007.onclick = function () {
+  inf007.classList.toggle('hide');
+};
+
+var but008 = document.querySelector('#d008');
+var inf008 = document.querySelector('.inf008');
+but008.onclick = function () {
+  inf008.classList.toggle('hide');
+};
+
+var but009 = document.querySelector('#d009');
+var inf009 = document.querySelector('.inf009');
+but009.onclick = function () {
+  inf009.classList.toggle('hide');
+};
+
+var but010 = document.querySelector('#d010');
+var inf010 = document.querySelector('.inf010');
+but010.onclick = function () {
+  inf010.classList.toggle('hide');
+};
+
+var but011 = document.querySelector('#d011');
+var inf011 = document.querySelector('.inf011');
+but011.onclick = function () {
+  inf011.classList.toggle('hide');
+};
+
+var but012 = document.querySelector('#d012');
+var inf012 = document.querySelector('.inf012');
+but012.onclick = function () {
+  inf012.classList.toggle('hide');
+};
+
+var but015 = document.querySelector('#d015');
+var inf015 = document.querySelector('.inf015');
+but015.onclick = function () {
+  inf015.classList.toggle('hide');
+};
+
+var but016 = document.querySelector('#d016');
+var inf016 = document.querySelector('.inf016');
+but016.onclick = function () {
+  inf016.classList.toggle('hide');
+};
+
+var but017 = document.querySelector('#d017');
+var inf017 = document.querySelector('.inf017');
+but017.onclick = function () {
+  inf017.classList.toggle('hide');
+};
+
+var but018 = document.querySelector('#d018');
+var inf018 = document.querySelector('.inf018');
+but018.onclick = function () {
+  inf018.classList.toggle('hide');
+};
+
+var but019 = document.querySelector('#d019');
+var inf019 = document.querySelector('.inf019');
+but019.onclick = function () {
+  inf019.classList.toggle('hide');
+};
+
+var but020 = document.querySelector('#d020');
+var inf020 = document.querySelector('.inf020');
+but020.onclick = function () {
+  inf020.classList.toggle('hide');
+};
+
+
